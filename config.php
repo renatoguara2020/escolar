17d659a1ca3daf9813910b7ce3d8eb49bd88969c
Renato Alves Soares <renatoguara2020@gmail.com>
2022-10-26 08:41:19 -0300

    commit changes

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+
+$nome_escola = "Escola Freitas";
+$url = "http://localhost/escola/";
+$endereco_escola = "Rua Alameda Campos, 157, Belo Horizonte";
+$telefone_escola = "(31)97527-5084";
+$email_adm = 'hvfadvocacia@gmail.com';
+$rodape_relatorios = "Desenvolvido por Hugo Vasconcelos - Portal Hugo Cursos";
+
+//VARIAVEIS DO BANCO DE DADOS LOCAL
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'escolar';
+
+ ?>
